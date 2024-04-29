6bd742790eb72321e2831e482344a0649904a96a
Ralkey <Ralkey@outlook.com>
2024-04-29 09:26:58 +0200

    fixed error when .mpd does not have audio

--- fansly_downloader.py
+++ fansly_downloader.py
@@ -701,16 +701,19 @@ def download_mpd(mpd_url: str, save_path: str):
     audio_representations = root.findall('.//{urn:mpeg:dash:schema:mpd:2011}AdaptationSet[@mimeType="audio/mp4"]/{urn:mpeg:dash:schema:mpd:2011}Representation')
 
     highest_quality_video = max(video_representations, key=lambda x: int(x.attrib['bandwidth']))
-    highest_quality_audio = max(audio_representations, key=lambda x: int(x.attrib['bandwidth']))
+    # check if audio exists before passing it along since it can lack audio
+    highest_quality_audio = None
+    if audio_representations:
+        highest_quality_audio = max(audio_representations, key=lambda x: int(x.attrib['bandwidth']))
 
     # Extract the BaseURLs from the highest quality video and audio representations
     video_base_url = highest_quality_video.find('{urn:mpeg:dash:schema:mpd:2011}BaseURL').text
-    audio_base_url = highest_quality_audio.find('{urn:mpeg:dash:schema:mpd:2011}BaseURL').text
+    audio_base_url = highest_quality_audio.find('{urn:mpeg:dash:schema:mpd:2011}BaseURL').text if highest_quality_audio else None
 
     # Construct the full URLs for the video and audio files
     base_url = '/'.join(mpd_url.split('/')[:-1]) + '/'
     video_url = base_url + video_base_url
-    audio_url = base_url + audio_base_url
+    audio_url = base_url + audio_base_url if audio_base_url else None
 
     # Create a hidden temp folder to store downloads in
     hidden_folder_dir = ".temp"
@@ -727,41 +730,49 @@ def download_mpd(mpd_url: str, save_path: str):
             os.mkdir(hidden_folder_dir)
 
     def download_file(url, file_path):
+        if url is None:
+            return
         res = sess.get(url, headers=headers, cookies=cookies, stream=True)
         res.raise_for_status()  # Raise an exception if the request fails
         with open(file_path, 'wb') as f:
             for chunk in res.iter_content(chunk_size=1024):
                 f.write(chunk)
 
-    # file names
-    video_file = "temp_video.mp4"
-    audio_file = "temp_audio.mp4"
-
     # hidden temp folder + file names
-    video_file_path = os.path.join(hidden_folder_dir, video_file)
-    audio_file_path = os.path.join(hidden_folder_dir, audio_file)
+    video_file_path = os.path.join(hidden_folder_dir, "temp_video.mp4")
+    audio_file_path = os.path.join(hidden_folder_dir, "temp_audio.mp4")
 
     # start download of the video and audio, and put it in the hidden folder
     download_file(video_url, video_file_path)
     download_file(audio_url, audio_file_path)
 
-    # combine the video and audio together into 1 file
-    # I am aware that using FFMPEG is not the best practice, however I don't know of any better alternative
-    ffmpeg = (
-        FFmpeg()
-        .option("y")
-        .input(video_file_path)
-        .input(audio_file_path)
-        .output(
-            save_path,
-            codec="copy",
+    # combine the video and audio together into 1 file IF both video and audio are present
+    if video_url and audio_url:
+        # I am aware that using FFMPEG is not the best practice, however I don't know of any better alternative
+        ffmpeg = (
+            FFmpeg()
+            .option("y")
+            .input(video_file_path)
+            .input(audio_file_path)
+            .output(
+                save_path,
+                codec="copy",
+            )
         )
-    )
-    ffmpeg.execute()
+        ffmpeg.execute()
+    elif video_url and not audio_url:  # else move the video in .temp folder to the normal path + rename it
+        os.rename(video_file_path, save_path)
 
-    # remove the video and audio file in the temp folder
-    os.remove(video_file_path)
-    os.remove(audio_file_path)
+    # remove the video and audio file in the temp folder after everything is done
+    try:  # if there is not a video file at video_file_path, continue the code without giving error
+        os.remove(video_file_path)
+    except OSError:
+        pass
+
+    try:  # if there is not a audio file at audio_file_path, continue the code without giving error
+        os.remove(audio_file_path)
+    except OSError:
+        pass
 
     return True
 
